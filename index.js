d42495deedb1912ec35ec734c8fdbb6dc190b9a5
excelitai-mahedi <mahedi@excelitai.com>
2022-06-01 11:29:37 +0600

    'testing'

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function verifyJWT(req,res,next) {
   const token =authHeader.split(' ')[1];
   jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded) => {
     if (err) {
-      return res.status(403).send({message:'Forbidden access'});
+      return res.status(403).send({message:'Forbidden access!!'});
     }
     console.log('decoded', decoded);
     req.decoded=decoded;
